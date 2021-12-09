1b8895e0fe3641114fb840acc476d533dbe673ca
ronya78@gmail.com <ronya78@gmail.com>
2021-12-09 02:02:04 +0200

    Install an NPM external modules in the project.

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 //jshint esversion:6
 
-const fs = require('fs');
+// const fs = require('fs');
+//
+// // file2.txt will be created or overwritten by default.
+// fs.copyFileSync('file1.txt', 'file2.txt');
+// console.log('file1.txt was copied to file2.txt')
 
-// file2.txt will be created or overwritten by default.
-fs.copyFileSync('file1.txt', 'file2.txt');
-console.log('file1.txt was copied to file2.txt');
+var superheroes = require('superheroes');
+var supervillains = require('supervillains');
+
+var mySuperheroName = superheroes.random();
+var mySupervillainsName = supervillains.random();
+
+console.log(mySuperheroName);
+console.log(mySupervillainsName);
